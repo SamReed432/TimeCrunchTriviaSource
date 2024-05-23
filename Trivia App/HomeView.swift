88267f41e65878e4323b3081dc08d0195b844075
Sam Reed <samreed12@att.net>
2024-05-23 16:12:35 -0400

    Removed ads

--- Trivia App/HomeView.swift
+++ Trivia App/HomeView.swift
@@ -33,41 +33,6 @@ enum HomeCancelID {
     case dailyTimer
 }
 
-struct BannerAdView: UIViewRepresentable {
-    var adUnitID: String
-
-    func makeCoordinator() -> Coordinator {
-        return Coordinator()
-    }
-
-    func makeUIView(context: Context) -> GADBannerView {
-        let bannerView = GADBannerView(adSize: GADAdSizeBanner)
-        bannerView.adUnitID = adUnitID
-        
-        if let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
-           let rootViewController = windowScene.windows.first?.rootViewController {
-            bannerView.rootViewController = rootViewController
-        }
-        
-        bannerView.delegate = context.coordinator
-        bannerView.load(GADRequest())
-        return bannerView
-    }
-
-    func updateUIView(_ uiView: GADBannerView, context: Context) {}
-
-    class Coordinator: NSObject, GADBannerViewDelegate {
-        func bannerViewDidReceiveAd(_ bannerView: GADBannerView) {
-            print("Banner loaded successfully")
-        }
-
-        func bannerView(_ bannerView: GADBannerView, didFailToReceiveAdWithError error: Error) {
-            print("Failed to load banner ad with error: \(error.localizedDescription)")
-        }
-    }
-}
-
-
 class SoundManager: ObservableObject {
     
     static let shared = SoundManager()
@@ -193,13 +158,6 @@ public struct HomeView: View {
                                 .fullScreenCover(isPresented: $views.isSheetPresented) {
                                     PopUpView(geometry: g)
                                 }
-                        }
-                        VStack {
-                            Spacer()
-                            BannerAdView(adUnitID: "ca-app-pub-4151998780971734/4463157793")
-                                .frame(maxWidth: .infinity) //g.size.width * 0.95)
-                                .frame(height: g.size.height * 0.1)
-                                .padding(.bottom, -g.size.height * 0.01)
                         }
                     }
                     
